b22abc4de4727e941fa1822fc57f09921b2d8b0f
Dippimunch <dippimunch@aim.com>
2021-03-02 03:17:02 -0500

    Add files via upload

--- climate.py
+++ climate.py
@@ -5,8 +5,11 @@ import random
 temp_min = 0
 temp_max = 100
 
-samples = 360
+samples = 20
 s = 0
+
+# day to begin
+time = 50
 
 # Tempeture range
 amplitude = 50
@@ -16,6 +19,7 @@ period = 360
 phase = 0
 # Midpoint
 vertical = 50
+# TODO: Latitude/ geographic variation
 
 location = None
 
@@ -24,10 +28,11 @@ class Climate:
         pass
 
 def get_weather(location, amplitude, period, phase, vertical, time):
-    variation = random.randrange(-20, 20)
+    variation = random.randrange(-15, 15)
     temp = amplitude * math.sin((2*math.pi/period)*(time-phase)) + vertical
     return temp + variation
     
 while s < samples:
-    print(get_weather(location, amplitude, period, phase, vertical, s))
+    print(get_weather(location, amplitude, period, phase, vertical, time))
+    time += 1
     s += 1
