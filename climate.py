25f2d9658bf89b660277bebedf8a54b45e85e842
Dippimunch <dippimunch@aim.com>
2021-03-02 03:07:24 -0500

    Add files via upload

--- climate.py
+++ climate.py
@@ -1,21 +1,33 @@
 import math
+import random
 
 
 temp_min = 0
 temp_max = 100
 
-samples = 180
+samples = 360
 s = 0
+
+# Tempeture range
+amplitude = 50
+# Length of year
+period = 360
+# Temperature at beginning of year
+phase = 0
+# Midpoint
+vertical = 50
 
 location = None
 
-def get_weather(location, time):
-    #print(math.sin(time))
-    #temp = (temp_max/2) * math.sin(time)
-    #print(math.radians(1))
-    temp = 50 * math.sin(math.radians(time))
-    return temp
+def class Climate:
+    def __init__(self, name, amplitude, period, phase, vertical):
+        pass
+
+def get_weather(location, amplitude, period, phase, vertical, time):
+    variation = random.randrange(-20, 20)
+    temp = amplitude * math.sin((2*math.pi/period)*(time-phase)) + vertical
+    return temp + variation
     
 while s < samples:
-    print(get_weather(location, s))
+    print(get_weather(location, amplitude, period, phase, vertical, s))
     s += 1
