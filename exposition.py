ea74620e48e5d3fbcf0828293571bee2214eeec9
Dippimunch <dippimunch@aim.com>
2022-06-11 23:25:22 -0400

    Expanded XSAMPA list and added IPA output

--- exposition.py
+++ exposition.py
@@ -9,8 +9,10 @@ location = Location('home', 1000, 'CCf', 100)
 player = Person('Name', 25, 'M', language, {}, location)
 print(f'you are with your kinfolk at {player.location.name}')
 #print(language.__dict__)
-print(dic.translate(f'you be with your human at {player.location.name}', language))
-dic.read_xsampa('R*aLRML g_<atfU*z*')
+text = dic.translate(f'you be with your human at {player.location.name}', language)
+print(text)
+print(dic.read_xsampa(text))
+
 # 2ndPerson be LOCATION locative KIN comitative
 
 
